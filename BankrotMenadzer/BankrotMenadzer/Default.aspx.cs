1abe5fe3f5f2d412f8cde8b7dfa3c3a6a7f96a23
Kristijan Spirovski <k_spirovski@hotmail.com>
2015-06-04 01:19:13 +0200

    Popravena metodata vo Default.aspx, za predhodnata verzija

--- BankrotMenadzer/BankrotMenadzer/Default.aspx.cs
+++ BankrotMenadzer/BankrotMenadzer/Default.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BankrotMenadzer;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Globalization;
@@ -12,13 +13,23 @@ namespace MacedonianRedCrossYouth
 {
     public partial class Default : System.Web.UI.Page
     {
-       /* protected void Page_Load(object sender, EventArgs e)
+       protected void Page_Load(object sender, EventArgs e)
         {
-            if (Session["user_id"] == null)
+            if (!IsPostBack)
             {
-                Response.Redirect("Login.aspx");
+                Database db = new Database();
+                DataSet ds = db.getAllCategories();
+                ddCategory.DataTextField = "name";
+                ddCategory.DataValueField = "category_id";
+
+                ddCategory.DataSource = ds;
+                ddCategory.DataBind();
             }
-        }*/
+            // if (Session["user_id"] == null)
+            // {
+            //    Response.Redirect("Login.aspx");
+            // }
+        }
 
         [WebMethod]
         public static string AJAX_AddTransaction()
@@ -27,9 +38,16 @@ namespace MacedonianRedCrossYouth
             return "{ \"label\": \"Test1\", \"value\": " + random + "}";
         }
 
-        private void dodadiTransakcija()
+        private void dodadiTransakcija(int type)
         {
-            double price = Convert.ToDouble(tbPrice.Text);
+            Database db = new Database();
+
+            bool wishlist = type == 3;
+            int price = Math.Abs(Convert.ToInt32(tbPrice.Text));
+            if (type != 1)
+            {
+                price *= -1;
+            }
             string name = tbName.Text;
             int category_id = 0;
             if (ddCategory.SelectedIndex != -1)
@@ -38,33 +56,43 @@ namespace MacedonianRedCrossYouth
             }
             DateTime datum = Convert.ToDateTime(tbDatum.Text);
             int comment_id = 0;
-            if (tbComment.Text != "")
+            // dali e ne prazen komentarot
+            if (!tbComment.Text.Trim().Equals(""))
             {
                 string comment = tbComment.Text;
+                comment_id = (int)db.addComment(comment);
                 //Dodavanje na komentar u baza
                 //Zemanje na indeks od baza za komentarot
-                //comment_id=
             }
+
             int user_id = int.Parse(Session["user_id"].ToString());
             //Funkcija za dodavanje u baza so parametrite od pogore
 
             //Ako e wishlist, dodavanje u tabela za wishlist kreiranata transakcija
+            if (comment_id != 0)
+            {
+                db.addTransaction(category_id, user_id, comment_id, price, datum, name, wishlist);
+            }
+            else
+            {
+                db.addTransactionBezKomentar(category_id, user_id, price, datum, name, wishlist);
+            }
+
         }
 
         protected void btnAddExpenditure_Click(object sender, ImageClickEventArgs e)
         {
-            dodadiTransakcija();
+            dodadiTransakcija(2);
         }
 
         protected void btnAddIncome_Click(object sender, ImageClickEventArgs e)
         {
-            dodadiTransakcija();
+            dodadiTransakcija(1);
         }
 
         protected void btnAddWish_Click(object sender, ImageClickEventArgs e)
         {
-            dodadiTransakcija();
-            
+            dodadiTransakcija(3);
         }
     }
 }
