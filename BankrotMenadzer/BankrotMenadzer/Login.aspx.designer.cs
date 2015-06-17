efdf512ebfbb39aa921704bf6054ac95933ba6d4
Kristijan Milanov <kristijanmilanov@gmail.com>
2015-06-17 20:27:27 +0200

    unique username, removeTransaction and wishlist
    Pri registracija napraveno e da ne moze da se dodade dvapati isto
    korisnicko ime. Vo History napravena e funkcija removeTransaction, treba
    da se napravi na klik na kopceto vo tabelata da se povikuva i vo baza da
    se napravi brisenjeto (napravena e funkcija vo Database koja se vika
    removeTransaction samo treba da se napise kodot za brisenje). Vo
    wishlist da se napravi repiter i da se modificira funkcijata od Database
    da vrakja List<Transaction> namesto DataTable.

--- BankrotMenadzer/BankrotMenadzer/Login.aspx.designer.cs
+++ BankrotMenadzer/BankrotMenadzer/Login.aspx.designer.cs
@@ -103,6 +103,15 @@ namespace BankrotManager {
         protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator2;
         
         /// <summary>
+        /// lblUsernameExists control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblUsernameExists;
+        
+        /// <summary>
         /// tbPasswordReg control.
         /// </summary>
         /// <remarks>
--- BankrotMenadzer/BankrotMenadzer/Wishlist.aspx.cs
+++ BankrotMenadzer/BankrotMenadzer/Wishlist.aspx.cs
@@ -9,12 +9,26 @@ namespace BankrotManager
 {
     public partial class Wishlist : System.Web.UI.Page
     {
+        private Database db = new Database();
         protected void Page_Load(object sender, EventArgs e)
         {
             if (Session["user_id"] == null)
             {
                 Response.Redirect("Login.aspx");
             }
+            ShowData();
+        }
+
+        private void ShowData()
+        {
+            var wishlist = db.getWishlist(int.Parse(Session["user_id"].ToString()));
+
+            //funkcijata za getWishlist da vrakja lista od transakcii
+            //da se dodade repiter za podatocite kako kaj History so moznost za brisenje (repiter_rawdata vo history)
+            //da se otkomentira kodot podolu za polnenje na podatocite vo tabelata
+
+            //this.repeater_rawdata.DataSource = wishlist;
+            //this.repeater_rawdata.DataBind();
         }
     }
 }
